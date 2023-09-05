caf8bc42365c18e556465b545480d76365e79737
Eric Salavatcioglu <eric.salavat@gmail.com>
2023-09-05 16:18:04 -0700

    Added Spring Boot initializer app code.

---
+++ src/main/java/com/salavatcioglu/apiresource/ApiResourceApplication.java
@@ -0,0 +1,13 @@
+package com.salavatcioglu.apiresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salavatcioglu/apiresource/ApiResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salavatcioglu.apiresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
